48aadfd6d6bfd6bbc24649ceba044924c026af2c
syedaairaj3-abedi <syedaairaj3@gmail.com>
2026-03-05 23:20:59 -0600

    Update extractor.py

--- src/extractor.py
+++ src/extractor.py
@@ -1,14 +1,15 @@
-import fitz
+import fitz  # PyMuPDF
 
-def extract_text_from_pdf(pdf_path):
-
+def extract_text_from_pdf(pdf_file):
     text = ""
 
-    doc = fitz.open(pdf_path)
+    pdf_bytes = pdf_file.read()
+    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
 
     for page in doc:
         text += page.get_text()
 
     doc.close()
+    pdf_file.seek(0)
 
     return text
