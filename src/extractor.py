b38b2b00c8f92c1226dda33ba83564da45c0014b
syedaairaj3-abedi <syedaairaj3@gmail.com>
2026-03-05 21:46:04 -0600

    Add PDF text extraction module

---
+++ src/extractor.py
@@ -0,0 +1,14 @@
+import fitz
+
+def extract_text_from_pdf(pdf_path):
+
+    text = ""
+
+    doc = fitz.open(pdf_path)
+
+    for page in doc:
+        text += page.get_text()
+
+    doc.close()
+
+    return text
